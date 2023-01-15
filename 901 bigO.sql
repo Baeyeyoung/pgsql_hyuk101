f831be298871e51b99411b93aa52cb9f6864f834
Wonhyuk Chung <myLoveSarah@gmail.com>
2023-01-16 08:09:43 +0900

    주석. 일부 데이터 변경

--- 901 bigO.sql
+++ 901 bigO.sql
@@ -1,3 +1,10 @@
+/*
+ * orders를 늘려 10,790개의 행을 만든다
+ * 일부 데이터를 조작하여 null로 바꾼다
+ * 인덱스를 만든다.
+ * 
+ * 정원혁 202211
+ */
 drop table if exists bigO;
 
 select * 
@@ -14,6 +21,26 @@ select customer_id, employee_id, order_date, required_date, shipped_date, ship_v
 from orders o ;
 select count(*) from bigO;
 
-select * from bigO;
+insert into bigO (customer_id, employee_id, order_date, required_date, shipped_date, ship_via, freight, ship_name, ship_address, ship_city, ship_region, ship_postal_code, ship_country)	select customer_id, employee_id, order_date, required_date, shipped_date, ship_via, freight, ship_name, ship_address, ship_city, ship_region, ship_postal_code, ship_country from orders o ;
+insert into bigO (customer_id, employee_id, order_date, required_date, shipped_date, ship_via, freight, ship_name, ship_address, ship_city, ship_region, ship_postal_code, ship_country)	select customer_id, employee_id, order_date, required_date, shipped_date, ship_via, freight, ship_name, ship_address, ship_city, ship_region, ship_postal_code, ship_country from orders o ;
+insert into bigO (customer_id, employee_id, order_date, required_date, shipped_date, ship_via, freight, ship_name, ship_address, ship_city, ship_region, ship_postal_code, ship_country)	select customer_id, employee_id, order_date, required_date, shipped_date, ship_via, freight, ship_name, ship_address, ship_city, ship_region, ship_postal_code, ship_country from orders o ;
+insert into bigO (customer_id, employee_id, order_date, required_date, shipped_date, ship_via, freight, ship_name, ship_address, ship_city, ship_region, ship_postal_code, ship_country)	select customer_id, employee_id, order_date, required_date, shipped_date, ship_via, freight, ship_name, ship_address, ship_city, ship_region, ship_postal_code, ship_country from orders o ;
+insert into bigO (customer_id, employee_id, order_date, required_date, shipped_date, ship_via, freight, ship_name, ship_address, ship_city, ship_region, ship_postal_code, ship_country)	select customer_id, employee_id, order_date, required_date, shipped_date, ship_via, freight, ship_name, ship_address, ship_city, ship_region, ship_postal_code, ship_country from orders o ;
+insert into bigO (customer_id, employee_id, order_date, required_date, shipped_date, ship_via, freight, ship_name, ship_address, ship_city, ship_region, ship_postal_code, ship_country)	select customer_id, employee_id, order_date, required_date, shipped_date, ship_via, freight, ship_name, ship_address, ship_city, ship_region, ship_postal_code, ship_country from orders o ;
+insert into bigO (customer_id, employee_id, order_date, required_date, shipped_date, ship_via, freight, ship_name, ship_address, ship_city, ship_region, ship_postal_code, ship_country)	select customer_id, employee_id, order_date, required_date, shipped_date, ship_via, freight, ship_name, ship_address, ship_city, ship_region, ship_postal_code, ship_country from orders o ;
+insert into bigO (customer_id, employee_id, order_date, required_date, shipped_date, ship_via, freight, ship_name, ship_address, ship_city, ship_region, ship_postal_code, ship_country)	select customer_id, employee_id, order_date, required_date, shipped_date, ship_via, freight, ship_name, ship_address, ship_city, ship_region, ship_postal_code, ship_country from orders o ;
+insert into bigO (customer_id, employee_id, order_date, required_date, shipped_date, ship_via, freight, ship_name, ship_address, ship_city, ship_region, ship_postal_code, ship_country)	select customer_id, employee_id, order_date, required_date, shipped_date, ship_via, freight, ship_name, ship_address, ship_city, ship_region, ship_postal_code, ship_country from orders o ;
+insert into bigO (customer_id, employee_id, order_date, required_date, shipped_date, ship_via, freight, ship_name, ship_address, ship_city, ship_region, ship_postal_code, ship_country)	select customer_id, employee_id, order_date, required_date, shipped_date, ship_via, freight, ship_name, ship_address, ship_city, ship_region, ship_postal_code, ship_country from orders o ;
+insert into bigO (customer_id, employee_id, order_date, required_date, shipped_date, ship_via, freight, ship_name, ship_address, ship_city, ship_region, ship_postal_code, ship_country)	select customer_id, employee_id, order_date, required_date, shipped_date, ship_via, freight, ship_name, ship_address, ship_city, ship_region, ship_postal_code, ship_country from orders o ;
 
-create index ixOrderDate on bigO (order_date);
+
+select min(order_id), max(order_id), count(*) from bigO;
+select * from bigO where order_id % 57 = 0;
+update bigO set order_date = null, ship_name = null where order_id % 57 = 0;
+
+
+select * from bigO where ship_name  is null;
+
+select * from bigO;
+create index ix_ship_name on bigO (ship_name);
+create index ix_orderDate on bigO (order_date);
