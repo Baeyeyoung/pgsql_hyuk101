67327234b2b988b1cb2bbdd3950b31b66f2bb37c
Wonhyuk Chung <myLoveSarah@gmail.com>
2023-01-22 08:43:14 +0900

    컬럼 테이블 명세서

--- 901 sp_help sp_tables.sql
+++ 901 sp_help sp_tables.sql
@@ -24,3 +24,56 @@ SELECT
    data_type 
 FROM 
    information_schema.columns;
+
+
+--테이블 명세서
+select	t.schemaname, tablename, tableowner, coalesce("tablespace",'') tablespace, hasindexes	--, hastriggers
+,	obj_description(oid) 
+from pg_tables t 
+	left join pg_class c on t.tablename::regclass::oid = c.oid
+where t.schemaname in('public')	
+order by tablename
+;
+--hastriggers는 오류
+
+
+
+--전체 테이블, 컬럼 명과 속성 정보를 보여준다
+--그래도 시트에 넣으면 컬럼 명세서로 사용가능하다.
+select c.table_schema, table_name, column_name, ordinal_position ord
+	, pgd.description
+	, udt_name, character_maximum_length, is_nullable, column_default
+	, is_identity
+	, numeric_precision, numeric_scale
+from information_schema."columns" c
+	left join pg_catalog.pg_statio_all_tables as st on  c.table_schema = st.schemaname and	c.table_name   = st.relname
+	left join pg_catalog.pg_description pgd on pgd.objoid = st.relid and pgd.objsubid = c.ordinal_position
+where  table_schema in('public')
+	--view 제외
+	and not exists (
+		select * from information_schema."views"
+		where table_schema in('public')
+			and table_name = c.table_name 
+	)	
+order by table_name, ordinal_position ;
+
+
+
+/*
+SELECT obj_description(oid), *
+FROM pg_class
+WHERE relkind = 'r'
+;
+
+
+select * from information_schema."tables" t ;
+
+select *, t.tablename::regclass::oid oid
+from pg_tables t
+where schemaname in('public')	
+order by t.tablename
+;
+ * 
+ */
+
+
