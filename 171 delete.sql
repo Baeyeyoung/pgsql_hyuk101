3335fcdbc6788e5460c9675cadf30c7c5ec80cb9
Wonhyuk Chung <myLoveSarah@gmail.com>
2023-01-22 10:10:02 +0900

    added 171

---
+++ 171 delete.sql
@@ -0,0 +1,36 @@
+/*
+	트랜잭션
+	for pgsql 2023.01
+	
+	https://www.postgresql.org/docs/current/transaction-iso.html
+*/
+
+/*
+ * 기본 DELETE
+ */
+drop table if exists o;
+SELECT * into o FROM orders;
+
+select * from o where order_id = 10248;
+
+DELETE from o 
+where order_id = 10248;
+
+select * from o where order_id = 10248;
+
+
+/*
+ * 좀 더 복잡한 DELETE
+*/
+select * from o 
+WHERE employee_id = 5
+	AND EXISTS (SELECT 1 FROM customers WHERE customer_id = o.customer_id AND customers.country = 'USA'); 
+
+delete from o
+WHERE employee_id = 5
+AND EXISTS (SELECT 1 FROM customers WHERE customer_id = o.customer_id AND customers.country = 'USA');
+
+select * from o 
+WHERE employee_id = 5
+	AND EXISTS (SELECT 1 FROM customers WHERE customer_id = o.customer_id AND customers.country = 'USA'); 
+
